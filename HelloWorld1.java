1b995ed8c771817ae2bd94385f1b0e7fa443aca8
jucheck <jucheck@jucheck_GL62M7RC>
2022-01-15 22:19:24 +0900

    1,3 수정해서 커밋

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -3,7 +3,7 @@ package company;
 public class HelloWorld1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("111 local에서 수정3");
 		System.out.println("111 local에서 수정2-서버");
 	}
 }
--- HelloWorld3.java
+++ HelloWorld3.java
@@ -4,6 +4,6 @@ public class HelloWorld3 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("44");
+		System.out.println("3-3");
 	}
 }
