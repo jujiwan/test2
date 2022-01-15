6666442ca28cb931f7b2ab8864e172416bca6969
jucheck <jucheck@jucheck_GL62M7RC>
2022-01-15 21:32:05 +0900

    3-1

--- HelloWorld3.java
+++ HelloWorld3.java
@@ -1,9 +1,9 @@
 package company;
 
-public class HelloWorld2 {
-
+public class HelloWorld3 {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("111");
+		System.out.println("44");
 	}
 }
