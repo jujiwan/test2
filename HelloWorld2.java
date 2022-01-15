7808ba09eb65ce6f792fa17387dce3f1d0378209
jucheck <jucheck@jucheck_GL62M7RC>
2022-01-15 20:36:43 +0900

    6반째 수정

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -6,6 +6,6 @@ public class HelloWorld2 {
 		// TODO Auto-generated method stub
 		System.out.println("JJW111a11");
 		System.out.println("JJW222b22");
-		System.out.println("JJW333c33");
+		System.out.println("JJW333c33"); 
 	}
 }
