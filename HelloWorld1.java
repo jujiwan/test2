915953486abefc87f698cf853325aca41a30b2f1
jucheck <jucheck@jucheck_GL62M7RC>
2022-01-15 21:37:12 +0900

    1

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -4,6 +4,6 @@ public class HelloWorld1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("111");
+		System.out.println("111 local에서 수정");
 	}
 }
