a972ad9cd5f43b6824b213d751a4a51ca9bacf55
jucheck <jucheck@jucheck_GL62M7RC>
2022-01-16 00:56:00 +0900

    1-77

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -15,5 +15,6 @@ public class HelloWorld1 {
 		System.out.println("111 local에서 수정7-서버");
 		System.out.println("111 local에서 수정6");
 		System.out.println("111 local에서 수정8-서버");
+		System.out.println("111 local에서 수정7");
 	}
 }
