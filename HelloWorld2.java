4ba0385d0819a0fc3e797b6edc55ee2c44bb59a8
jucheck <jucheck@jucheck_GL62M7RC>
2022-01-15 20:22:26 +0900

    4번째

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -4,8 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("JJW111a1");
-		System.out.println("JJW222b2");
-		System.out.println("JJW333c3");
+		System.out.println("JJW111a11");
+		System.out.println("JJW222b22");
+		System.out.println("JJW333c33");
 	}
 }
