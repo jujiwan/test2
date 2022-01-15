ae9cca905ecad868ac81babfe54d1ff2c3ec389c
jucheck <jucheck@jucheck_GL62M7RC>
2022-01-15 20:13:02 +0900

    처음 로컬에서 수정함

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("JJW");
+		System.out.println("JJW111");
+		System.out.println("JJW222");
+		System.out.println("JJW333");
 	}
-
 }
