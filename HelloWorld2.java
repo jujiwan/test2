5e3916b8d2d4a3491458ba5a46f7e15fa655091a
jujiwan <97773894+jujiwan@users.noreply.github.com>
2022-01-15 20:11:44 +0900

    Update HelloWorld2.java

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -4,7 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("JJW");
+		System.out.println("JJW1");
+		System.out.println("JJW2");
+		System.out.println("JJW3");
 	}
 
 }
